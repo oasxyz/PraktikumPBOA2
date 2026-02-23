b9e44c79244682ef8c3f5c45a12e192daed97d59
romualdus yoas wicaksono <wicaksonoyoas@gmail.com>
2026-02-23 14:01:30 +0700

    Titik

---
+++ Pertemuan1/src/Titik.java
@@ -0,0 +1,36 @@
+public class Titik {
+    // Atribut
+    double absis;
+    double ordinat;
+
+    // Konstruktor
+    Titik() {
+        absis = 0;
+        ordinat = 0;
+    }
+
+    double getAbsis() {
+        return absis;
+    }
+
+    double getOrdinat() {
+        return ordinat;
+    }
+
+    void setAbsis(double x) {
+        absis = x;
+    }
+
+    void setOrdinat(double y) {
+        ordinat = y;
+    }
+
+    void geser(double x, double y) {
+        absis = absis + x;
+        ordinat = ordinat + y;
+    }
+
+    void printTitik() {
+        System.out.println("Titik(" + absis + ", " + ordinat + ")");
+    }
+}
